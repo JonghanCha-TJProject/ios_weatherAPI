8a66402db823716afca6742f2bce610d510f6b78
jonghancha <jonghancha@gmail.com>
2021-03-15 17:52:53 +0900

    03-15 종한 : 끝

---
+++ LocalWeather_json/LocalWeather_json/JsonModel.swift
@@ -0,0 +1,133 @@
+//
+//  JsonModel.swift
+//  LocalWeather_json
+//
+//  Created by TJ on 2021/03/11.
+//
+
+import Foundation
+
+protocol JsonModelProtocol: class {
+    func locationDownloaded(items: NSArray)
+    func weatherDownloaded(items: NSArray)
+}
+
+class JsonModel: NSObject{
+    var delegate: JsonModelProtocol! // ! 못불러올 수도 있으니까
+    var urlPath = "https://www.metaweather.com/api/location/search/?query=se"
+    
+    func downloadItems(){
+        let url = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil{
+                print("Failed to download data")
+            }else{
+                print("Data is downloading")
+                    self.parseJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    
+    func downloadWeatherItems(woeid: Int){
+        urlPath = "https://www.metaweather.com/api/location/\(woeid)/"
+        print(urlPath)
+        let url = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil{
+                print("Failed to download data")
+            }else{
+                print("Data is downloading")
+                self.parseWeatherJSON(data!)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+    
+    /*
+     json parsing 작업
+     */
+    func parseJSON(_ data: Data){
+        var jsonResult = NSArray()
+        
+        do{
+            jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSArray // json model 탈피
+        }catch let error as NSError{
+            print(error)
+        }
+        
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray() // 딕셔너리에서 값만 써주기 위해
+        
+        for i in 0..<jsonResult.count {
+            jsonElement = jsonResult[i] as! NSDictionary
+            
+            if let title = jsonElement["title"] as? String,// let에 정상적으로 들어왔을 때.
+               let woeid = jsonElement["woeid"] as? Int{
+
+                let query = LocationInfo(locationName: title, woeid: woeid)
+                
+                locations.add(query)
+            }
+//            locations.add(query)
+        } // for End.
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate.locationDownloaded(items: locations)
+        })
+    }
+    
+    
+    func parseWeatherJSON(_ data: Data){
+         var jsonResult = NSArray()
+        
+        do{
+            var jsonFieldElement = NSDictionary()
+            jsonFieldElement = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments) as! NSDictionary
+            if let consolidatedWeather = jsonFieldElement["consolidated_weather"] as? NSArray{
+                jsonResult = consolidatedWeather
+            }
+        }catch let error as NSError{
+            print(error)
+        }
+        
+        var jsonElement = NSDictionary()
+        let locations = NSMutableArray() // 딕셔너리에서 값만 써주기 위해
+        
+      
+        
+        for i in 0..<jsonResult.count {
+            jsonElement = jsonResult[i] as! NSDictionary
+            
+            if let id = jsonElement["id"] as? Int,// let에 정상적으로 들어왔을 때.
+               let weatherStateName = jsonElement["weather_state_name"] as? String,
+               let weatherStateAbbr = jsonElement["weather_state_abbr"] as? String,
+               let theTemp = jsonElement["the_temp"] as? Double,
+               let humidity = jsonElement["humidity"] as? Float{
+                
+                let query = WeatherInfo(id: id, weatherStateName: weatherStateName, weatherStateAbbr: weatherStateAbbr, theTemp: Int(theTemp), humidity: Int(humidity))
+                locations.add(query)
+                
+            }
+//            locations.add(query)
+        } // for End.
+        DispatchQueue.main.async(execute: {() -> Void in
+            self.delegate.weatherDownloaded(items: locations)
+        })
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}// ----
---
+++ LocalWeather_json/LocalWeather_json/WeatherViewController.swift
@@ -0,0 +1,166 @@
+//
+//  WeatherViewController.swift
+//  LocalWeather_json
+//
+//  Created by TJ on 2021/03/11.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, JsonModelProtocol   {
+   
+    
+   
+
+    @IBOutlet weak var myActivityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableViewWeather: UITableView!
+    
+    
+    var locationList: Array<String>  = Array<String>()
+    var woeidList: Array<Int> = Array<Int>()
+    
+    var todayList: Array<WeatherInfo>  = Array<WeatherInfo>()
+    var tomorrowList: Array<WeatherInfo>  = Array<WeatherInfo>()
+    
+    var count = 0
+    
+//    var todayWeather: NSArray = NSArray()
+//    var tomorrowWeather: NSArray = NSArray()
+    
+    private var refreshControl = UIRefreshControl()
+        
+    
+    @objc func refresh(refresh: UIRefreshControl){
+        
+        refresh.endRefreshing()
+        self.tableViewWeather.reloadData() // Reload
+    }
+        
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableViewWeather.delegate = self
+        self.tableViewWeather.dataSource = self
+        
+        
+        let jsonModel = JsonModel()
+        jsonModel.delegate = self
+        jsonModel.downloadItems()
+        
+        myActivityIndicator.stopAnimating()
+        myActivityIndicator.isHidden = true
+        
+        tableViewWeather.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+       
+        // Do any additional setup after loading the view.
+        // 그림 크기
+//        tableViewWeather.rowHeight = 105// Cell 높이. <----------- 중요
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    
+   
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return 1
+        default:
+            return locationList.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell", for: indexPath) as! HeaderTableViewCell
+            tableView.rowHeight = 43.6
+            return cell
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath) as! WeatherTableViewCell
+            // Configure the cell...
+            // 셀 구성 정해주기
+//            let item: WeatherInfo = todayWeather[indexPath.row] as! WeatherInfo
+           
+            tableView.rowHeight = 105
+            cell.labelLocation?.text = locationList[indexPath.row] as! String
+            cell.labelStateToday?.text = todayList[indexPath.row].weatherStateName as! String
+            cell.labelTempToday?.text = String(todayList[indexPath.row].theTemp!) + "℃"
+            cell.labelHumidityToday?.text = String(todayList[indexPath.row].humidity!) + "%"
+
+            
+            var url = URL(string: "https://www.metaweather.com/static/img/weather/png/64/\(todayList[indexPath.row].weatherStateAbbr!).png")
+            
+            var data = try? Data(contentsOf: url!)
+            cell.imageViewToday.image = UIImage(data: data!)
+            
+            cell.labelStateTomorrow?.text = todayList[indexPath.row].weatherStateName as! String
+            cell.labelTempTomorrow?.text = String(tomorrowList[indexPath.row].theTemp!) + "℃"
+            cell.labelHumidityTomorrow?.text = String(tomorrowList[indexPath.row].humidity!) + "%"
+            
+            
+            url = URL(string: "https://www.metaweather.com/static/img/weather/png/64/\(tomorrowList[indexPath.row].weatherStateAbbr!).png")
+            data = try? Data(contentsOf: url!)
+            cell.imageViewTomorrow.image = UIImage(data: data!)
+            
+            return cell
+        }
+        
+    }
+    
+    func locationDownloaded(items: NSArray) {
+        var beanList = Array<LocationInfo>()
+        beanList = items as! [LocationInfo]
+        
+        for item in 0...(items.count-1) {
+            locationList.append(beanList[item].locationName!)
+            woeidList.append(beanList[item].woeid!)
+        }
+        
+        
+        for item in 0...(locationList.count - 1) {
+            let jsonModel = JsonModel()
+            jsonModel.delegate = self
+            jsonModel.downloadWeatherItems(woeid: woeidList[item])
+        }
+        
+        
+        
+    }
+    
+    func weatherDownloaded(items: NSArray) {
+        var beanList = Array<WeatherInfo>()
+        beanList = items as! [WeatherInfo]
+        todayList.append(beanList[0])
+        tomorrowList.append(beanList[1])
+        count += 1
+        if count == 12 {
+            print(12)
+            
+            self.tableViewWeather.reloadData()
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
